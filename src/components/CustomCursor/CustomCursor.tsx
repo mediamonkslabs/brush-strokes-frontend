cffb03cd1540edf91d4348bf2d75a2995165925a
Stuart van Beek <art0rz@protonmail.com>
2019-09-17 11:56:54 -0400

    separate loading spinner SVG from cursor SVG. Loading spinner is moved to the bottom right of the screen when touch is detected

--- src/components/CustomCursor/CustomCursor.tsx
+++ src/components/CustomCursor/CustomCursor.tsx
@@ -31,25 +31,53 @@ const CustomCursor = ({
   const [progressLerp, setProgressLerp] = useState<ReturnType<typeof startLerp> | null>(null);
 
   const cursorRef = createRef<SVGSVGElement>();
+  const spinnerRef = createRef<SVGSVGElement>();
   const [isPointerVisible, setPointerVisible] = useState<boolean>(false);
+  const [isTouch, setIsTouch] = useState<boolean>(false);
 
   const pointerMoveCallback = useCallback(
     (event: PointerEvent) => {
+      setIsTouch(event.pointerType === 'touch');
       if (cursorRef.current !== null) {
         cursorRef.current.style.setProperty('left', `${event.offsetX + cursorOffsetX}px`);
         cursorRef.current.style.setProperty('top', `${event.offsetY + cursorOffsetY}px`);
       }
+      if (spinnerRef.current !== null) {
+        if (event.pointerType !== 'touch') {
+          spinnerRef.current.style.setProperty('left', `${event.offsetX + cursorOffsetX}px`);
+          spinnerRef.current.style.setProperty('top', `${event.offsetY + cursorOffsetY}px`);
+        } else {
+          spinnerRef.current.style.removeProperty('left');
+          spinnerRef.current.style.removeProperty('top');
+        }
+      }
     },
-    [cursorRef, cursorOffsetX, cursorOffsetY],
+    [cursorRef, cursorOffsetX, cursorOffsetY, spinnerRef, isTouch],
   );
 
-  const pointerLeaveCallback = useCallback(() => {
-    setPointerVisible(false);
-  }, []);
+  const pointerLeaveCallback = useCallback(
+    (event: PointerEvent) => {
+      setIsTouch(event.pointerType === 'touch');
+      setPointerVisible(false);
+      if (spinnerRef.current !== null) {
+        spinnerRef.current.style.removeProperty('left');
+        spinnerRef.current.style.removeProperty('top');
+      }
+    },
+    [spinnerRef],
+  );
 
-  const pointerEnterCallback = useCallback(() => {
-    setPointerVisible(true);
-  }, []);
+  const pointerEnterCallback = useCallback(
+    (event: PointerEvent) => {
+      setIsTouch(event.pointerType === 'touch');
+      if (event.pointerType === 'touch' && spinnerRef.current !== null) {
+        spinnerRef.current.style.removeProperty('left');
+        spinnerRef.current.style.removeProperty('top');
+      }
+      setPointerVisible(true);
+    },
+    [spinnerRef],
+  );
 
   const setOutlineRadius = useCallback(
     p => {
@@ -122,6 +150,25 @@ const CustomCursor = ({
           [styles.cursorVisible]: isPointerVisible,
         })}
       >
+        <path
+          d="M43.2 53c-.5-.5-.4-1.3.1-1.8l21.8-20c1.1-1 2.8-1 3.8 0s1 2.6 0 3.7L48.3 56.2c-.5.5-1.3.6-1.8.1L43.2 53zm-.5 4.5c2.5 2.5 2.5 6.5 0 9-1.5 1.4-3.9 2-6.2 2.3-2.1.2-6 .6-6 .6s.4-3.8.6-5.9c.3-2.2.9-4.6 2.3-6 2.7-2.4 6.8-2.4 9.3 0"
+          fill="none"
+          stroke="#000"
+          strokeWidth=".956"
+        />
+      </svg>
+      <svg
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        ref={spinnerRef}
+        className={classNames(styles.spinner, {
+          [styles.spinnerTouch]: isTouch,
+          [styles.spinnerVisible]: isTouch || isPointerVisible,
+        })}
+      >
         <circle
           cx="50"
           cy="50"
@@ -144,13 +191,6 @@ const CustomCursor = ({
           strokeWidth="3"
           fill="transparent"
           strokeDasharray={circumference.toString()}
-        />
-
-        <path
-          d="M43.2 53c-.5-.5-.4-1.3.1-1.8l21.8-20c1.1-1 2.8-1 3.8 0s1 2.6 0 3.7L48.3 56.2c-.5.5-1.3.6-1.8.1L43.2 53zm-.5 4.5c2.5 2.5 2.5 6.5 0 9-1.5 1.4-3.9 2-6.2 2.3-2.1.2-6 .6-6 .6s.4-3.8.6-5.9c.3-2.2.9-4.6 2.3-6 2.7-2.4 6.8-2.4 9.3 0"
-          fill="none"
-          stroke="#000"
-          strokeWidth=".956"
         />
       </svg>
       {children}
